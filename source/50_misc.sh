1500e9d474b8c2289c8d6f88848b9d7a206ccc56
Marcus Ramberg <marcus.ramberg@usit.uio.no>
2015-05-07 10:30:30 +0200

    Add some neat functions, new muttrc for local, vimrc

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -16,3 +16,29 @@ bindkey '\e.' insert-last-word
 alias sudo='sudo '
 # Fix up last command
 alias fuck='sudo $(fc -ln -1)'
+
+function flip() {
+  perl -C3 -Mutf8 -lpe '$_=reverse;y/a-zA-Z.['\'',({?!\"<_;‿⁅∴\r/ɐqɔpǝɟƃɥıɾʞ|ɯuodbɹsʇnʌʍxʎzɐqɔpǝɟƃɥıɾʞ|ɯuodbɹsʇnʌʍxʎz˙],'\'')}¿¡,>‾؛⁀⁆∵\n/' <<< "$1"
+}
+
+function fuck() {
+  CMD="pkill"
+  which $CMD >/dev/null || CMD="killall"
+  echo
+  if [ "$1" "==" "off" ]; then
+    shift
+    FLIP=(' (ノಠ益ಠ)ノ彡' '(ノಠ-ಠ)ノ彡')
+    SIG="-9"
+  else
+    [ "$1" "==" "you" ] && shift
+    FLIP=(' (╯°□°）╯︵' '(ノ ゜Д゜)ノ ︵' '(ﾉಥДಥ)ﾉ︵' ' ヽ(`Д´)ﾉ︵' ' (ﾉಥ益ಥ）ﾉ ')
+    SIG=""
+  fi
+  [ -z "$1" ] && { echo "┬─┬﻿ ノ( ゜-゜ノ) patience young grasshopper"; echo; return; }
+  F=${FLIP[$RANDOM % ${#FLIP[@]} ]}
+  if $CMD $SIG "$1"; then
+    echo "$F$(flip $1)"; echo
+  else
+    echo "┬─┬﻿ ︵ /(.□. \）"; echo
+  fi
+}
