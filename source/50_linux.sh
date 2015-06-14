f2f19001c3fc710718b4576423ff47d5861377e3
Marcus Ramberg <marcus.ramberg@usit.uio.no>
2015-06-14 18:20:44 +0200

    xc, remove obsolete linux code

--- source/50_linux.sh
+++ source/50_linux.sh
@@ -1,6 +1,7 @@
-# Ubuntu-only stuff. Abort if not Ubuntu.
+# Linux-only stuff. Abort if not Linux 
 [[ "$(uname)" =~ Linux ]] || return 1
 
 export PATH=~/.dotfiles/bin/linux:$PATH
 
 eval $(dircolors ~/.dircolors)
+alias xc=xclip -selection c
