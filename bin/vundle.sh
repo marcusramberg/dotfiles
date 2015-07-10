9ca67401f829e01e80924e11f334e8d22dcd998c
Marcus Ramberg <marcus.ramberg@usit.uio.no>
2015-07-10 11:45:53 +0200

    try to run more non-interactive

--- bin/vundle.sh
+++ bin/vundle.sh
@@ -7,4 +7,4 @@ then
   git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
   echo "\n\tRead about vim configuration for vundle at https://github.com/gmarik/vundle\n"  
 fi
-vim -c "execute \"PluginInstall\" | q | q"
+vim -c "execute \"PluginInstall\" | q | q" > /dev/null
