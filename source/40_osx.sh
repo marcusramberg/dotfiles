0888839b0dff8862f7102327858d0c9f473070cc
Marcus Ramberg <marcus.ramberg@gmail.com>
2016-09-05 15:52:17 +0200

    Make sure osx runs before devel

---
+++ source/40_osx.sh
@@ -0,0 +1,103 @@
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+# APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
+PATH=/usr/local/bin:$(path_remove /usr/local/bin)
+PATH=/usr/local/sbin:$(path_remove /usr/local/sbin)
+PATH=~/.dotfiles/bin/osx:$(path_remove ~/.dotfiles/bin/osx)
+export PATH
+
+export DOCKER_TLS_VERIFY=1
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/marcus/.boot2docker/certs/boot2docker-vm
+export GOPATH=/Users/marcus/gocode
+export GOROOT=/usr/local/Cellar/go/1.5.3/libexec
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export JAVA_HOME="$(/usr/libexec/java_home)"
+
+
+# Trim new lines and copy to clipboard
+alias c="tr -d '\n' | pbcopy"
+
+# Fix open with
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+
+
+# Make 'less' more.
+eval "$(lesspipe.sh)"
+
+# Start ScreenSaver. This will lock the screen if locking is enabled.
+alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+
+# Create a new Parallels VM from template, replacing the existing one.
+function vm_template() {
+  local name="$@"
+  local basename="$(basename "$name" ".zip")"
+  local dest_dir="$HOME/Documents/Parallels"
+  local dest="$dest_dir/$basename"
+  local src_dir="$dest_dir/Templates"
+  local src="$src_dir/$name"
+  if [[ ! -n "$name" || ! -e "$src" ]]; then
+    echo "You must specify a valid VM template from this list:";
+    for f in "$src_dir"/*.pvm "$src_dir"/*.pvm.zip; do
+      echo " * $(basename "$f")"
+    done
+    return 1
+  fi
+  if [[ -e "$dest" ]]; then
+    echo "Deleting old VM"
+    rm -rf "$dest"
+  fi
+  echo "Restoring VM template"
+  if [[ "$name" == "$basename" ]]; then
+    cp -R "$src" "$dest"
+  else
+    unzip -q "$src" -d "$dest_dir" && rm -rf "$dest_dir/__MACOSX"
+  fi && \
+  echo "Starting VM" && \
+  open -g "$dest"
+}
+
+# Export Localization.prefPane text substitution rules.
+function txt_sub_backup() {
+  local prefs=~/Library/Preferences/.GlobalPreferences.plist
+  local backup=~/.dotfiles/conf/osx/NSUserReplacementItems.plist
+  /usr/libexec/PlistBuddy -x -c "Print NSUserReplacementItems" "$prefs" > "$backup" &&
+  echo "File ~${backup#$HOME} written."
+}
+
+# Import Localization.prefPane text substitution rules.
+function txt_sub_restore() {
+  local prefs=~/Library/Preferences/.GlobalPreferences.plist
+  local backup=~/.dotfiles/conf/osx/NSUserReplacementItems.plist
+  if [[ ! -e "$backup" ]]; then
+    echo "Error: file ~${backup#$HOME} does not exist!"
+    return 1
+  fi
+  cmds=(
+    "Delete NSUserReplacementItems"
+    "Add NSUserReplacementItems array"
+    "Merge '$backup' NSUserReplacementItems"
+  )
+  for cmd in "${cmds[@]}"; do /usr/libexec/PlistBuddy -c "$cmd" "$prefs"; done
+}
+
+function zle-keymap-select zle-line-init zle-line-finish {
+  case $KEYMAP in
+    vicmd)      print -n -- "\E]50;CursorShape=0\C-G";; # block cursor
+    viins|main) print -n -- "\E]50;CursorShape=1\C-G";; # line cursor
+  esac
+
+  zle reset-prompt
+  zle -R
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+
+eval $(gdircolors ~/.dircolors)
+
+source "`brew --prefix`/etc/grc.bashrc"
+
+eval $(docker-machine env)
