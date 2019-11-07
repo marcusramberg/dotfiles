2805763447779f97046ed5f30ab2fed26dc8f825
Marcus Ramberg <marcus.ramberg@gmail.com>
2019-11-07 11:08:43 +0100

    Restore karabiner setup

--- link/hammerspoon/init.lua
+++ link/hammerspoon/init.lua
@@ -1,4 +1,6 @@
-k = hs.hotkey.modal.new({}, "F17")
+-- hyper = {"cmd","alt","ctrl"}
+hyper = {"cmd","alt","ctrl","shift"}
+
 
 local ipc = require("hs.ipc")
 local tiling = require "hs.tiling"
@@ -10,32 +12,32 @@ local spaces = require("hs._asm.undocumented.spaces")
 
 
 
-k:bind({}, "w", function() tiling.cycleLayout() ; k.triggered=true end)
-k:bind({}, "j", function() tiling.cycle(1) ; k.triggered=true end)
-k:bind({}, "k", function() tiling.cycle(-1) ; k.triggered=true end)
-k:bind({}, 'y', hs.toggleConsole)
-k:bind({}, 'n', function() hs.task.new("/usr/bin/open", nil, {os.getenv("HOME")}):start() ; k.triggered=true end)
+hs.hotkey.bind(hyper, "w", function() tiling.cycleLayout()  end)
+hs.hotkey.bind(hyper, "j", function() tiling.cycle(1)  end)
+hs.hotkey.bind(hyper, "k", function() tiling.cycle(-1) end)
+hs.hotkey.bind(hyper, 'y', hs.toggleConsole)
+hs.hotkey.bind(hyper, 'n', function() hs.task.new("/usr/bin/open", nil, {os.getenv("HOME")}):start() end)
 
-k:bind({}, "space", function() tiling.promote() ; k.triggered=true end)
-k:bind({}, "m", function() tiling.goToLayout("fullscreen") ; k.triggered=true end)
-k:bind({}, "r", function() hs.reload() ; k.triggered=true end)
-k:bind({}, "right", function() hs.screen:next() ; k.triggered=true end)
-k:bind({}, "left", function() hs.screen:next() ; k.triggered=true end)
-k:bind({}, "return", function() hs.application.launchOrFocus("iTerm") ; k.triggered=true end)
-k:bind({}, "f", function()
-      hs.hints.windowHints(); k.triggered=true 
+hs.hotkey.bind(hyper, "space", function() tiling.promote() end)
+hs.hotkey.bind(hyper, "m", function() tiling.goToLayout("fullscreen") end)
+hs.hotkey.bind(hyper, "r", function() hs.reload() end)
+hs.hotkey.bind(hyper, "right", function() hs.screen:next() end)
+hs.hotkey.bind(hyper, "left", function() hs.screen:next() end)
+hs.hotkey.bind(hyper, "return", function() hs.application.launchOrFocus("iTerm 2") end)
+hs.hotkey.bind(hyper, "f", function()
+      hs.hints.windowHints()
     end)
  hints.style = "vimperator"
 
-k:bind({},"1", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(1)'"); k.triggered=true end)
-k:bind({},"2", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(2)'"); k.triggered=true end)
-k:bind({},"3", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(3)'"); k.triggered=true end)
-k:bind({},"4", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(4)'"); k.triggered=true end)
-k:bind({},"5", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(5)'"); k.triggered=true end)
-k:bind({},"6", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(6)'"); k.triggered=true end)
-k:bind({},"7", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(7)'"); k.triggered=true end)
-k:bind({},"8", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(8)'"); k.triggered=true end)
-k:bind({},"9", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(9)'"); k.triggered=true end)
+hs.hotkey.bind(hyper,"1", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(1)'") end)
+hs.hotkey.bind(hyper,"2", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(2)'") end)
+hs.hotkey.bind(hyper,"3", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(3)'") end)
+hs.hotkey.bind(hyper,"4", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(4)'") end)
+hs.hotkey.bind(hyper,"5", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(5)'") end)
+hs.hotkey.bind(hyper,"6", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(6)'") end)
+hs.hotkey.bind(hyper,"7", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(7)'") end)
+hs.hotkey.bind(hyper,"8", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(8)'") end)
+hs.hotkey.bind(hyper,"9", function() os.execute("ruby -rtotalspaces2 -e'TotalSpaces2.move_to_space(9)'") end)
 
 -- Callback function for application events
 function applicationWatcher(appName, eventType, appObject)
@@ -57,9 +59,7 @@ local function newChromeWindow()
   visibleWindows[2]:focus()
 end
 
-k:bind({}, "c", function()
-  newChromeWindow(); k.triggered=true 
-end)
+hs.hotkey.bind(hyper, "c", newChromeWindow)
 
 
 hs.notify.new({
@@ -108,11 +108,10 @@ local spacesEventtap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, functi
   return true
 end):start()
 
-k:bind({}, "e", function()
+hs.hotkey.bind(hyper, "e", function()
   -- this is to bind the spacesEventtap variable to a long-lived function in
   -- order to prevent GC from doing their evil business
   hs.alert.show("Fast space switching enabled: " .. tostring(spacesEventtap:isEnabled()))
-  k.triggered=true 
 end)
 
 -- Wifi
@@ -123,20 +122,3 @@ function ssidChangedCallback()
     end
 end
 
--- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k.triggered = false
-  k:enter()
-end
-
--- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
---   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
-  k:exit()
-  if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
-  end
-end
-
--- Bind the Hyper key
-f18 = hs.hotkey.bind("", 'F18', pressedF18, releasedF18)
