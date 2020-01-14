2b8260d9c72990e6c64ad8562bee52bf29895ea1
Marcus Ramberg <Marcus.Ramberg@dnb.no>
2020-01-14 08:37:59 +0100

    Add URLDispatcher to set browser defaults for certain patterns, fall back to browsersaurus for now

--- link/hammerspoon/init.lua
+++ link/hammerspoon/init.lua
@@ -17,12 +17,30 @@ hs.hotkey.bind(hyper, 'n', function() hs.task.new("/usr/bin/open", nil, {os.gete
 hs.hotkey.bind(hyper, "t", function() hs.application.launchOrFocus("iTerm") end)
 hs.hotkey.bind(hyper, "b", function() hs.application.launchOrFocus("Vivaldi") end)
 
-
-
-
+hs.window.animationDuration = 0.1
 
 hs.ipc.cliInstall()
 
+
+-- Load SpoonInstall, so we can easily load our other Spoons
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall.use_syncinstall = true
+Install=spoon.SpoonInstall
+
+
+-- Direct URLs automatically based on patterns
+Install:andUse("URLDispatcher",
+  {
+    config = {
+      url_patterns = {
+        { "https?://.*.redhat.com", "org.mozilla.firefox" },
+        { "https?://microsoft.com/devicelogin", "com.google.Chrome" },
+      },
+      default_handler = "com.browserosaurus"
+    },
+    start = true
+  }
+)
 
 
 -- EOF
