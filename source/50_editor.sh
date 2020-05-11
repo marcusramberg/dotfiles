ceca05a53b5720e9be903c53bb788a35a524bb78
Marcus Ramberg <Marcus.Ramberg@dnb.no>
2020-05-11 19:31:17 +0200

    fixes

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,13 +1,16 @@
 # Editing
 
+export BAT_THEME="Nord"
+alias less="bat"
+alias cat="bat"
 alias q='nvim'
-alias vi='vim'
+alias vi='emacsclient -n'
 type nvim &> /dev/null && alias vim=nvim
 if [[ ! -n "$SSH_TTY" ]]; then
-  export EDITOR='nvim'
+  export EDITOR='emacsclient'
   export LESSEDIT='nvim %f'
 else
-  export EDITOR=$(type nvim vim vi nano pico 2>/dev/null | sed 's/ .*$//;q')
+  export EDITOR=$(type emacsclient nvim vim vi nano pico 2>/dev/null | sed 's/ .*$//;q')
   alias q="$EDITOR -w -z"
 fi
 
