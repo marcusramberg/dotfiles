6a9efdf39e86ea102f1e2cafc8719e35d2f10208
Marcus Ramberg <Marcus.Ramberg@dnb.no>
2021-01-08 09:50:24 +0100

    Update hammerspoon config

--- link/hammerspoon/init.lua
+++ link/hammerspoon/init.lua
@@ -24,7 +24,7 @@ hs.hotkey.bind(hyper, 'c', hs.toggleConsole)
 hs.hotkey.bind(hyper, "r", function() hs.reload() end)
 hs.hotkey.bind(hyper, 'n', function() hs.task.new("/usr/bin/open", nil, {os.getenv("HOME")}):start() end)
 hs.hotkey.bind(hyper, "t", function() hs.application.launchOrFocus("iTerm") end)
-hs.hotkey.bind(hyper, "b", function() hs.application.launchOrFocus("Vivaldi") end)
+hs.hotkey.bind(hyper, "b", function() hs.application.launchOrFocus("Firefox Developer Edition") end)
 
 
 
@@ -38,19 +38,18 @@ Install=spoon.SpoonInstall
 
 
 -- Direct URLs automatically based on patterns
-Install:andUse("URLDispatcher",
-  {
-    config = {
-      url_patterns = {
-        { "https?://.*.tech-01.net", "com.vivaldi.Vivaldi" },
-        { "https?://microsoft.com/devicelogin", "com.vivaldi.Vivaldi" },
-      },
-      default_handler = "org.browserosaurus"
-      --default_handler = "org.mozilla.firefoxdeveloperedition"
-    },
-    start = true
-  }
-)
+-- Install:andUse("URLDispatcher",
+--   {
+--     config = {
+--       url_patterns = {
+--         { "https?://.*.tech-01.net", "com.vivaldi.Vivaldi" },
+--         { "https?://microsoft.com/devicelogin", "com.vivaldi.Vivaldi" },
+--       },
+--       default_handler = "org.mozilla.firefoxdeveloperedition"
+--     },
+--     start = true
+--   }
+-- )
 
 
 -- EOF
@@ -58,4 +57,3 @@ hs.notify.new({
     title='Hammerspoon',
     informativeText='Config loaded'
   }):send()
-
