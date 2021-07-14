7f2292b873644682a73e36f4542c4834ffd589fa
Marcus Ramberg <marcus@means.no>
2021-07-14 18:45:10 +0200

    More linux aliases

--- source/50_linux.sh
+++ source/50_linux.sh
@@ -5,3 +5,24 @@ export PATH=~/.dotfiles/bin/linux:$PATH
 
 eval $(dircolors ~/.dircolors)
 alias xc=xclip -selection c
+alias icat="kitty +kitten icat"
+alias pai="pamac install"
+alias pas="pamac search"
+export BROWSER='firefox-developer-edition'
+GPG_TTY=$(tty)
+export GPG_TTY
+source /usr/share/doc/find-the-command/ftc.zsh
+
+export GOPATH=~/go
+export PATH=$PATH:$GOPATH/bin:~/.emacs.d/bin/
+alias icat="kitty +kitten icat"
+alias pai="pamac install"
+alias pas="pamac search"
+export BROWSER='firefox-developer-edition'
+GPG_TTY=$(tty)
+export GPG_TTY
+source /usr/share/doc/find-the-command/ftc.zsh
+
+export GOPATH=~/go
+export PATH=$PATH:$GOPATH/bin:~/.emacs.d/bin/
+
