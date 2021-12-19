5ff68ca7d581ccac72ba9a3828301bafc67686e0
Marcus Ramberg <marcus@means.no>
2021-12-19 22:33:56 +0100

    Add ripgrep config, wezterm config

---
+++ link/config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+return {
+  adjust_window_size_when_changing_font_size = false,
+  audible_bell = "Disabled",
+  color_scheme = "Dracula+",
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  pane_focus_follows_mouse = true,
+  window_decorations = "RESIZE"
+}
+
