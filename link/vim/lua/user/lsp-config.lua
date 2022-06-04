d48b91fddb0e81eab7bed839515d5a93c984dff5
Marcus Ramberg <marcus@means.no>
2022-06-05 00:58:11 +0200

    Use cmp for capabilities

--- link/vim/lua/user/lsp-config.lua
+++ link/vim/lua/user/lsp-config.lua
@@ -41,20 +41,7 @@ local on_attach_vim = function(client)
 
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = {
-		'documentation',
-		'detail',
-		'additionalTextEdits',
-	}
-}
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
