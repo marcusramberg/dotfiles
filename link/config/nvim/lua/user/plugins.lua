dc35cec2a4b3db13167056db789f7b2a0a99071e
Marcus Ramberg <marcus@means.no>
2022-06-05 11:58:43 +0200

    Make dash mac only

--- link/config/nvim/lua/user/plugins.lua
+++ link/config/nvim/lua/user/plugins.lua
@@ -140,10 +140,12 @@ packer.startup(function()
 		requires = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-nvim-lsp-signature-help', 'hrsh7th/cmp-nvim-lua', 'hrsh7th/cmp-vsnip', 'hrsh7th/vim-vsnip', 'hrsh7th/vim-vsnip-integ', },
 	}
   use { 'nvim-lua/lsp-status.nvim'}
-  use({
-      'mrjones2014/dash.nvim',
-      run = 'make install',
-    })
+  if(jit.os == "OSX") then
+    use({
+        'mrjones2014/dash.nvim',
+        run = 'make install',
+      })
+  end
 
 
 --   use 'nvim-lua/popup.nvim'
