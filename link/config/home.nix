075b026aea78cac157d2dc71f802e2518f13d73e
Marcus Ramberg <marcus@means.no>
2022-09-11 18:38:27 +0200

    Giving flakes & nix-darwin a go

---
+++ link/config/home.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.stateVersion = "22.05";
+
+  # https://github.com/malob/nixpkgs/blob/master/home/default.nix
+
+  # Direnv, load and unload environment variables depending on the current directory.
+  # https://direnv.net
+  # https://rycee.gitlab.io/home-manager/options.html#opt-programs.direnv.enable
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  # Htop
+  # https://rycee.gitlab.io/home-manager/options.html#opt-programs.htop.enable
+  programs.htop.enable = true;
+  programs.htop.settings.show_program_path = true;
+
+  home.packages = with pkgs; [
+    # Some basics
+    coreutils
+    curl
+    deadnix
+    wget
+
+    # Dev stuff
+    # (agda.withPackages (p: [ p.standard-library ]))
+    google-cloud-sdk
+    jq
+    nodePackages.typescript
+    nodejs
+
+    # Useful nix related tools
+    cachix # adding/managing alternative binary caches hosted by Cachix
+    comma # run software from without installing it
+    niv # easy dependency management for nix projects
+    nodePackages.node2nix
+
+  ] ++ lib.optionals stdenv.isDarwin [
+    cocoapods
+    m-cli # useful macOS CLI commands
+  ];
+
+  # Misc configuration files --------------------------------------------------------------------{{{
+
+  # # https://docs.haskellstack.org/en/stable/yaml_configuration/#non-project-specific-config
+  # home.file.".stack/config.yaml".text = lib.generators.toYAML {} {
+  #   templates = {
+  #     scm-init = "git";
+  #     params = {
+  #       author-name = "Marcus Ramberg"; # config.programs.git.userName;
+  #       author-email = "marcus.ramberg@remarkable.com"; # config.programs.git.userEmail;
+  #       github-username = "marcusramberg";
+  #     };
+  #   };
+    # nix.enable = true;
+  # };
+}
