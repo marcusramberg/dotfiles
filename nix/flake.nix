45fa2dc379ab9373a9eaea07d8f5e7bb5a52a88e
Marcus Ramberg <marcus@means.no>
2023-01-23 00:10:31 +0100

    Port fish config to nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -11,65 +11,65 @@
     darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    # nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
 
     # Other sources
-    comma.url = github:nix-community/comma; 
+    comma.url = github:nix-community/comma;
     devenv.url = "github:cachix/devenv/v0.2";
 
 
 
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, devenv, nix-doom-emacs, ... } @inputs:
-  let 
-    inherit (darwin.lib) darwinSystem;
-    inherit (inputs.nixpkgs-unstable.lib) attrValues optionalAttrs singleton;
+  outputs = { self, darwin, nixpkgs, home-manager, devenv, ... } @inputs:
+    let
+      inherit (darwin.lib) darwinSystem;
+      inherit (inputs.nixpkgs-unstable.lib) attrValues optionalAttrs singleton;
 
 
-    # Configuration for `nixpkgs`
-    nixpkgsConfig = {
-      config = { allowUnfree = true; };
-      overlays = attrValues self.overlays ++ singleton (
-        # Sub in x86 version of packages that don't build on Apple Silicon yet
-        final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          inherit (final.pkgs-x86)
-            idris2
-            nix-index
-            devenv
-            niv;
-        })
-      );
-    }; 
-  in
-  {
-    # My `nix-darwin` configs
-      
-    darwinConfigurations = rec {
-      mbook = darwinSystem {
-        system = "aarch64-darwin";
-        modules = attrValues self.darwinModules ++ [ 
-          # Main `nix-darwin` config
-          ./configuration.nix
-          # `home-manager` module
-          home-manager.darwinModules.home-manager
-          {
-            nixpkgs = nixpkgsConfig;
-            inherit nix-doom-emacs;
-            # `home-manager` config
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.marcus = import ./home.nix;
-          }
-        ];
+      # Configuration for `nixpkgs`
+      nixpkgsConfig = {
+        config = { allowUnfree = true; };
+        overlays = attrValues self.overlays ++ singleton (
+          # Sub in x86 version of packages that don't build on Apple Silicon yet
+          final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+            inherit (final.pkgs-x86)
+              idris2
+              nix-index
+              devenv
+              niv;
+          })
+        );
       };
-    };
+    in
+    {
+      # My `nix-darwin` configs
 
-    # Overlays --------------------------------------------------------------- {{{
+      darwinConfigurations = rec {
+        mbook = darwinSystem {
+          system = "aarch64-darwin";
+          modules = attrValues self.darwinModules ++ [
+            # Main `nix-darwin` config
+            ./configuration.nix
+            # `home-manager` module
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs = nixpkgsConfig;
+              # inherit nix-doom-emacs;
+              # `home-manager` config
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.marcus = import ./home.nix;
+            }
+          ];
+        };
+      };
 
-    overlays = {
+      # Overlays --------------------------------------------------------------- {{{
 
-      # Overlay useful on Macs with Apple Silicon
+      overlays = {
+
+        # Overlay useful on Macs with Apple Silicon
         apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
           # Add access to x86 packages system is running Apple Silicon
           pkgs-x86 = import inputs.nixpkgs-unstable {
@@ -77,30 +77,30 @@
             inherit (nixpkgsConfig) config;
           };
 
-        }; 
+        };
       };
 
-    # My `nix-darwin` modules that are pending upstream, or patched versions waiting on upstream
-    # fixes.
-    darwinModules = {
-      programs-nix-index = 
-        # Additional configuration for `nix-index` to enable `command-not-found` functionality with Fish.
-        { config, lib, pkgs, ... }:
+      # My `nix-darwin` modules that are pending upstream, or patched versions waiting on upstream
+      # fixes.
+      darwinModules = {
+        programs-nix-index =
+          # Additional configuration for `nix-index` to enable `command-not-found` functionality with Fish.
+          { config, lib, pkgs, ... }:
 
-        {
-          config = lib.mkIf config.programs.nix-index.enable {
-            programs.fish.interactiveShellInit = ''
-              function __fish_command_not_found_handler --on-event="fish_command_not_found"
-                ${if config.programs.fish.useBabelfish then ''
-                command_not_found_handle $argv
-                '' else ''
-                ${pkgs.bashInteractive}/bin/bash -c \
-                  "source ${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh; command_not_found_handle $argv"
-                ''}
-              end
-            '';
+          {
+            config = lib.mkIf config.programs.nix-index.enable {
+              programs.fish.interactiveShellInit = ''
+                function __fish_command_not_found_handler --on-event="fish_command_not_found"
+                  ${if config.programs.fish.useBabelfish then ''
+                  command_not_found_handle $argv
+                  '' else ''
+                  ${pkgs.bashInteractive}/bin/bash -c \
+                    "source ${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh; command_not_found_handle $argv"
+                  ''}
+                end
+              '';
             };
-        };
+          };
+      };
     };
- };
 }
--- nix/home.nix
+++ nix/home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 {
   home.stateVersion = "22.05";
-  imports = [ nix-doom-emacs.hmModule ];
+  # imports = [ nix-doom-emacs.hmModule ];
 
 
   # https://github.com/malob/nixpkgs/blob/master/home/default.nix
@@ -21,14 +21,83 @@
     settings.show_program_path = true;
   };
 
-  programs.doom-emacs = {
+  #  programs.doom-emacs = {
+  #    enable = true;
+  #    doomPrivateDir = ./doom.d; # Directory containing your config.el, init.el
+  #    # and packages.el files
+  #  };
+  programs.fish = {
     enable = true;
-    doomPrivateDir = ./doom.d; # Directory containing your config.el, init.el
-    # and packages.el files
+    functions = {
+      fish_greeting = "fortune art goedel wisdom tao literature songs-poems paradoxum; echo ''";
+      rd = "fd $argv (git root)";
+    };
+    shellAbbrs = {
+      l = "ls";
+      gc = "git commit";
+      gs = "git status";
+      gsu = "git status -uno";
+      gp = "git pull";
+      gb = "git branch";
+      gd = "git diff";
+      gwa = "git  worktree add";
+      gwl = "git  worktree list";
+      gwr = "git  worktree remove";
+      gl = "git log";
+      vi = "nvim";
+      gfu = "git fetch --all --prune && git rebase origin/main";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      k = "kubectl ";
+      kx = "kubectx";
+      kns = "kubens";
+    };
+    shellAliases = {
+      cat = "bat";
+      ls = "exa";
+      ag = "rg";
+      emacs = "emacs -nw";
+      r = "cd (git root)";
+      imgcat = "wezterm imgcat";
+    };
+    shellInit = ''
+      fish_add_path /.dotfiles/bin /usr/local/sbin 
+      fish_add_path -a /run/current_system/sw/bin ~/.local/bin /opt/homebrew/bin ~/go/bin/ ~/.nimble/bin
+      set CLOUDSDK_PYTHON_SITEPACKAGES 1
+    '';
+    interactiveShellInit = ''
+      fish_vi_key_bindings
+      set fish_theme  tokyo-night
+      set -gx EDITOR nvim
+      type -q thefuck; and thefuck --alias | source
+      test -x ~/.plenv/bin/plenv; and . (~/.plenv/bin/plenv init -|psub)
+      if [ -f '/Users/marcus/google-cloud-sdk/path.fish.inc' ]; . '/Users/marcus/google-cloud-sdk/path.fish.inc'; end
+
+      # Completion
+      type -q kustomize; and eval (kustomize completion fish)
+      type -q yq; and yq shell-completion fish | source
+      type -q nvm; and nvm use -s
+
+      source ~/.config/fish/tide_config.fish
+    '';
+    loginShellInit = ''
+      if [ -f /Users/marcus/.ssh/id_rsa ]
+        ssh-add -q --apple-use-keychain  ~/.ssh/id_rsa
+        ssh-add -q --apple-use-keychain  ~/.ssh/id_dsa
+        ssh-add -q --apple-use-keychain  ~/.ssh/google_compute_engine
+      end
+      set -x GPG_TTY (tty)
+      gpgconf --launch gpg-agent
+    '';
   };
+  programs.atuin.enable = true;
+  programs.fzf.enable = true;
+  programs.keychain.enable = true;
+  programs.navi.enable = true;
+  programs.zoxide.enable = true;
 
   home.packages = with pkgs; [
-
     actionlint
     bat
     cocogitto
@@ -39,13 +108,13 @@
     git
     git-crypt
     fd
-    fzf
     fortune
     go
     golangci-lint
     gopls
     jq
     nodePackages.typescript
+    nodePackages.node2nix
     nodejs-16_x
     neovim-unwrapped
     ripgrep
@@ -53,15 +122,14 @@
     wget
     yq-go
 
-    # Useful nix related tools
-    cachix # adding/managing alternative binary caches hosted by Cachix
+    # useful nix related tools
+    cachix # adding/managing alternative binary caches hosted by cachix
     comma # run software from without installing it
     niv # easy dependency management for nix projects
-    nodePackages.node2nix
-
   ] ++ lib.optionals stdenv.isDarwin [
     cocoapods
     m-cli # useful macOS CLI commands
   ];
 
 }
+
