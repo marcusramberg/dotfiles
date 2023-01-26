dc3f66adfc4784d615330c68504750be06363636
Marcus Ramberg <marcus@means.no>
2023-01-26 22:50:07 +0100

    Fix build deps for nixos, setup tmux

--- nix/home.nix
+++ nix/home.nix
@@ -27,14 +27,43 @@
   #    # and packages.el files
   #  };
   programs.fish = import ./home/fish.nix { inherit pkgs; };
-  programs.fzf.enable = true;
+  programs.fzf = {
+    enable = true;
+    tmux.enableShellIntegration = true;
+  };
   programs.keychain.enable = true;
   programs.navi.enable = true;
   programs.zoxide.enable = true;
 
+  programs.tmux = {
+    aggressiveResize = true;
+    baseIndex = 1;
+    clock24 = true;
+    enable = true;
+    keyMode = "vi";
+    prefix = "`";
+    secureSocket = false; # survives user logout
+    terminal = "tmux-256color";
+    tmuxinator.enable = true;
+    extraConfig = ''
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -g set-titles on
+      set -g set-titles-string "#I > #T"
+      set -g status-style bg=white,fg=black
+      set -g mode-style bg=white,fg=black
+      set -g message-style bg=white,fg=black
+      set -g message-command-style bg=white,fg=black
+      set -g status off
+      set -g display-time 1000
+      set-window-option -g visual-bell on
+      set-window-option -g bell-action other
+    '';
+  };
+
   home.packages = with pkgs; [
     actionlint
     bat
+    cargo
     cocogitto
     coreutils
     curl
@@ -44,15 +73,19 @@
     git-crypt
     fd
     fortune
+    gnumake
     go
     golangci-lint
     gopls
+    go-task
     gotestfmt
+    grc
     jq
     nodePackages.typescript
     nodePackages.node2nix
     nodejs-16_x
     neovim-unwrapped
+    (import ./home/python.nix { inherit pkgs; })
     ripgrep
     tealdeer
     wget
