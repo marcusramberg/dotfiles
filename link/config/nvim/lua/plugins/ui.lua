ab9e98a703b2011946b6b32e6a290f8d4dfbe822
Marcus Ramberg <marcus@means.no>
2023-01-26 19:25:23 +0100

    Pimp my lazyvim

--- link/config/nvim/lua/plugins/ui.lua
+++ link/config/nvim/lua/plugins/ui.lua
@@ -52,4 +52,24 @@ return {
   },
   -- Scope buffers to tabs
   { "tiagovla/scope.nvim", config = true },
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      local logo = [[
+SSSSS                                       .sSSS s.
+SSSSS      .sSSSSs.   SSSSSSSSSs..sSSS SSSSSSSSSS SSSs.SSSSS.sSSSsSS SSsSSSSS
+S SSS      S SSSSSSSs.SSSSSS SSS'S SSS SSSSSS SSS SSSSSS SSSS SSS  SSS  SSSSS
+S  SS      S  SS SSSSS    S  SS  S  SS SSSSSS  SS SSSSSS  SSS  SS   S   SSSSS
+S..SS      S..SSsSSSSS   S..SS   `..SSsSSSS'S..SS SSSSSS..SSS..SS       SSSSS
+S:::S      S:::S SSSSS  S:::S       S:::S    S::S SSSS S:::SS:::S       SSSSS
+S;;;S      S;;;S SSSSS S;;;S        S;;;S     S;S SSS  S;;;SS;;;S       SSSSS
+S%%%S SSSSSS%%%S SSSSSS%%%SSSSSSS   S%%%S      SS SS   S%%%SS%%%S       SSSSS
+SSSSSsSS;:'SSSSS SSSSSSSSSSSSSSSS   SSSSS       SsS    SSSSSSSSSS       SSSSS
+]]
+
+      local dash = opts
+      dash.section.header.val = vim.split(logo, "\n")
+      return opts
+    end,
+  },
 }
